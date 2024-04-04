ac3ba53220338dbef944251a64fcc5ae38e201ff
Alfrey-Chan <122188063+Alfrey-Chan@users.noreply.github.com>
2024-04-04 13:34:09 -0700

    Place business logic to ClothingController

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ClothingController;
 use OpenAI\Laravel\Facades\OpenAI;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Route;
@@ -7,35 +8,37 @@ use Illuminate\Support\Facades\Request;
 use Illuminate\Support\Facades\Storage;
 use App\Http\Controllers\OpenAIServiceController;
 
-Route::get('/ai', function() {
+Route::get('/ai', [ClothingController::class, 'processImages']);
 
-    $baseURL = 'https://github.com/Alfrey-Chan/Project-backend/blob/main/public/clothing_images/';
-    $imageURL = "{$baseURL}cream_pullover_hoodie.png?raw=true";
+// Route::get('/ai', function() {
+
+//     $baseURL = 'https://github.com/Alfrey-Chan/Project-backend/blob/main/public/clothing_images/';
+//     $imageURL = "{$baseURL}cream_pullover_hoodie.png?raw=true";
     
 
-    $response = OpenAI::chat()->create([
-        'model' => 'gpt-4-vision-preview',
-        'max_tokens' => 100,
-        'messages' => [
-            [
-                'role' => 'user',
-                'content' => [
-                    [
-                        'type' => 'image_url',
-                        'image_url' => [
-                            'url' => $imageURL,
-                            'detail' => 'low'
-                        ]
-                    ]
-                ]
-            ],
-            [
-                'role' => 'user',
-                'content' => 'Describe the image for me in one sentence'
-            ]
-        ]
-    ]); 
+//     $response = OpenAI::chat()->create([
+//         'model' => 'gpt-4-vision-preview',
+//         'max_tokens' => 100,
+//         'messages' => [
+//             [
+//                 'role' => 'user',
+//                 'content' => [
+//                     [
+//                         'type' => 'image_url',
+//                         'image_url' => [
+//                             'url' => $imageURL,
+//                             'detail' => 'low'
+//                         ]
+//                     ]
+//                 ]
+//             ],
+//             [
+//                 'role' => 'user',
+//                 'content' => 'Describe the image for me in one sentence'
+//             ]
+//         ]
+//     ]); 
 
-    echo $response->choices[0]->message->content;
-}); 
+//     echo $response->choices[0]->message->content;
+// }); 
     
